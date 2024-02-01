fd21f302603885a8a72059b93e0db81a6b32c4ce
PedroMozany <76888414+PedroMozany@users.noreply.github.com>
2024-02-01 14:44:36 -0300

    creat project

---
+++ src/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="main-banner">
+    <h1>Make Your Burger</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+};
+</script>
+
+<style>
+#main-banner{
+  background-image: url("../components/img/banner.jpg");
+  background-position:  0 -610px;
+  background-size: cover;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+#main-banner h1{
+  color: #fff;
+  text-align: center;
+  font-size: 60px;
+  background-color: rgba(72, 61, 61, 0.4);
+  padding: 20px 40px;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer id="footer">
+        <p> Make Your Burger &copy; 2024</p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+#footer{
+  height: 200px;
+  background-color: #222;
+  border: 4px solid #111;
+  color: #FCBA03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0;
+  width: 100%;
+}
+</style>
